27368f88060c224858a85d70cf28b6e89c46f2ad
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-07-19 02:48:23 +0000

    Fixing attribute reference.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@988 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/rest_endpoint.rb
+++ lib/cg_service_client/rest_endpoint.rb
@@ -30,9 +30,9 @@ module CgServiceClient
           raise CgServiceClient::Exceptions::ServerError.new(response.code, response.status_message), "Server error #{response.code}: #{response.body}."
         elsif response.code == 0
           # no http response
-          raise CgServiceClient::Exceptions::ConnectionError.new(response.curl_error_code, response.curl_error_message), response.curl_error_message
+          raise CgServiceClient::Exceptions::ConnectionError.new(response.curl_return_code, response.curl_error_message), response.curl_error_message
         elsif response.timed_out?
-          raise CgServiceClient::Exceptions::TimeoutError.new(response.curl_error_code, response.curl_error_message), "Request for #{request_url} timed out."
+          raise CgServiceClient::Exceptions::TimeoutError.new(response.curl_return_code, response.curl_error_message), "Request for #{request_url} timed out."
         else
           raise CgServiceClient::Exceptions::ConnectionError.new(response.code, response.body), "Request for #{request_url} failed."
         end
