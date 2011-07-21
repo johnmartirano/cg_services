addfa56fd6cc54cc457967327b184c52f7726e19
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-07-21 14:36:29 +0000

    Added back in refresh thread
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@1135 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/serviceable.rb
+++ lib/cg_service_client/serviceable.rb
@@ -2,22 +2,31 @@ require 'cg_lookup_client'
 
 module CgServiceClient
   module Serviceable
-    @endpoint = nil
+    ENDPOINT_FLUSH_INTERVAL_IN_SEC = 60
 
     attr_reader :endpoint
 
     def uses_service(service_name, service_version, endpoint_class)
-      if @endpoint.nil?
-        @endpoint = find_service_endpoint(service_name, service_version, endpoint_class)
-      end
+      @service_name = service_name
+      @service_version = service_version
+      @endpoint_class = endpoint_class
+      @endpoint = find_service_endpoint
     end
 
-    def find_service_endpoint(service_name, service_version, endpoint_class)
-      result = CgLookupClient::Entry.lookup(service_name, service_version)
+    def find_service_endpoint
+      result = CgLookupClient::Entry.lookup(@service_name, @service_version)
       if result.nil? || result[:entry].nil?
         raise ServiceUnavailableError, "No #{name} services are available."
       else
-        eval(endpoint_class).new(result[:entry].uri, service_version)
+        eval(@endpoint_class).new(result[:entry].uri, @service_version)
+      end
+    end
+
+      # This thread periodically clears out the endpoint reference, thereby forcing it to be resolved again.
+    Thread.new do
+      loop do
+        sleep ENDPOINT_FLUSH_INTERVAL_IN_SEC
+        @endpoint = find_service_version
       end
     end
 
