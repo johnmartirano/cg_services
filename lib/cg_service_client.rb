3d8d7d6752ea724f285f41a6308218a4a2fc9b60
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-07-21 03:13:45 +0000

    Added cg_lookup_client gem.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@1123 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client.rb
+++ lib/cg_service_client.rb
@@ -1,9 +1,9 @@
 # This first block is required for Ruby 1.8.7. Once we've upgraded to 1.9.x,
 # it can be removed.
 require 'rubygems'
-#gem 'activemodel', '>=3.0.0'
-gem 'typhoeus', '>=0.2.4'
 
+gem 'typhoeus', '>=0.2.4'
+gem 'cg_lookup_client', '>=0.5.0'
 
 require 'cg_service_client/exceptions.rb'
 require 'cg_service_client/rest_endpoint'
