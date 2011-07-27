7dadd7a986799102ee5db67665c62523b951e151
John Martirano <john.martirano@commongroundpublishing.com>
2011-07-27 19:21:51 +0000

    Another refactor into CgService - configure Sinatra Reloader for environment
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/services/cg_service/trunk@1383 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service.rb
+++ lib/cg_service.rb
@@ -7,7 +7,6 @@ module CgService
   module RakeLoader
     class << self
       def load_tasks!
-        puts 'boo!'
         require 'rake'
         [:db].each do |file|
           load(File.join(File.dirname(__FILE__), "tasks/#{file.to_s}.rake"))
@@ -84,6 +83,19 @@ module CgService
   
       end
     end
+
+	# Configure sinatra reloader for a particular environment, defaults to development.
+    def configure_sinatra_reloader(enviro=:development)
+      configure(enviro) do
+        begin
+          require 'sinatra/reloader'
+          register Sinatra::Reloader
+        rescue LoadError
+          puts("Install sinatra-reloader gem.")
+        end
+      end
+    end
+
   end
 
 end
