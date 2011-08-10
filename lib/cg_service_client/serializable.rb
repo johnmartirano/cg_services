1dc2136137d2d4c0e0b0109f200d1eca203a20b9
Patrick Mahoney <patrick.mahoney@commongroundpublishing.com>
2011-08-10 16:41:21 +0000

    Add serializable mixin using ActiveModel's JSON serializer.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@1924 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/serializable.rb
+++ lib/cg_service_client/serializable.rb
@@ -1,18 +1,49 @@
 require 'active_model'
-require 'active_support'
 require 'active_support/hash_with_indifferent_access'
 
 module CgServiceClient
+  # Mixin to give #as_json, #to_json, and #from_json methods.
+  # Attributes that should be serialized must be created with
+  # serializable_attr_accessor.
   module Serializable
-    extend ActiveModel::Serializers::JSON
+    def self.included(receiver)
+      receiver.extend ClassMethods
 
-    include_root_in_json = false
+      # Can't simply include this inside Serializable because of
+      # stuff done by JSON on include (needs a Class, not Module)
+      receiver.send(:include, ActiveModel::Serializers::JSON)
+      receiver.send(:include_root_in_json=, false)
+    end
 
+    module ClassMethods
+      def attribute_keys
+        @attribute_keys ||= []
+      end
+
+      # Define getter and setter methods for each +sym+.  Also make
+      # these serializable to and from JSON.
+      def serializable_attr_accessor(*syms)
+        attr_accessor(*syms)
+        attribute_keys.push(*syms)
+      end
+    end
+
+    # Get the keys for serializeable attributes
+    def attribute_keys
+      self.class.attribute_keys
+    end
+
+    # Used by ActiveModel::Serialization do get the keys that should
+    # go into the serialized representation.
     def attributes
-      ATTRIBUTES.inject(
+      # Note: the values returned here are ignored by
+      # ActiveModel::Serializers#serializable_hash; just the keys are
+      # used to again call those getter methods...  But we do want to
+      # check the values so we can leave 'nil' out of the hash.
+      attribute_keys.inject(
           ActiveSupport::HashWithIndifferentAccess.new) do |result, key|
-        attribute_value = read_attribute_for_validation(key)
-        result[key] = attribute_value unless attribute_value == nil
+        value = send(key)
+        result[key] = value unless value == nil
         result
       end
     end
@@ -20,12 +51,5 @@ module CgServiceClient
     def attributes=(attrs)
       attrs.each_pair { |k, v| send("#{k}=", v) }
     end
-
-      # Helper to get the value of a particular attribute.
-    def read_attribute_for_validation(key)
-      send(key)
-    end
-
   end
-
 end
