406d772418fa006ac004b06bbc7da8e9e09c103f
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-08-11 02:43:15 +0000

    Can now query for activities using allows?
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_role_client/trunk@1959 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_role_client/aggregate_role.rb
+++ lib/cg_role_client/aggregate_role.rb
@@ -1,7 +1,28 @@
+require 'set'
+
 module CgRoleClient
   class AggregateRole
+
     def initialize(roles=[])
       @roles = roles
+      @activities = Set.new
+      roles.each do |role|
+        role.role_type.activities.each do |activity|
+          @activities.add(activity.code)
+        end
+      end
+    end
+
+    def allows?(activity)
+      @activities.include?(activity.code)
+    end
+
+    def activities
+      @activities
+    end
+
+    def roles
+      @roles
     end
   end
 end
