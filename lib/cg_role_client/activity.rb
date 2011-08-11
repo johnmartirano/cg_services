dcac6769dd028aaa095f64347d21213db2c9f380
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-08-11 02:42:23 +0000

    Added eql and hash methods
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_role_client/trunk@1957 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_role_client/activity.rb
+++ lib/cg_role_client/activity.rb
@@ -7,12 +7,12 @@ module CgRoleClient
 
   class Activity
     include ActiveModel::Validations
-    extend CgServiceClient::Serviceable
     include CgServiceClient::Serializable
+    extend CgServiceClient::Serviceable
 
     serializable_attr_accessor :id, :code, :name, :created_at, :updated_at
 
-    uses_service("Role","1","CgRoleClient::RestEndpoint")
+    uses_service("Role", "1", "CgRoleClient::RestEndpoint")
 
     class << self
       def endpoint
@@ -27,6 +27,20 @@ module CgRoleClient
 
     def initialize(attributes = {})
       self.attributes = attributes
+    end
+
+    def eql?(object)
+      if object.equal?(self)
+        return true
+      elsif !self.class.equal?(object.class)
+        return false
+      end
+
+      object.code.eql? code
+    end
+
+    def hash
+      code.hash
     end
 
   end
