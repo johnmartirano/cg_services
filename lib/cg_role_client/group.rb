4e5abb3df2c9609bb350ed90189d0931a077fba2
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-08-12 16:18:52 +0000

    Filling out group model
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_role_client/trunk@2047 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_role_client/group.rb
+++ lib/cg_role_client/group.rb
@@ -6,13 +6,63 @@ module CgRoleClient
 
   class Group
     include ActiveModel::Validations
+    include Aspect4r # this has to be here for the class level "around" to work
     include CgServiceClient::Serializable
+    extend CgServiceClient::Serviceable
+
+    uses_service("Role", "1", "CgRoleClient::RestEndpoint")
 
     serializable_attr_accessor :id, :code, :name, :created_at, :updated_at
+
+    validates_presence_of :code, :name
+
+    class << self
+      include Aspect4r
+
+      around :create, :find_by_code, :method_name_arg => true do |method, *args, &block |
+        begin
+          ensure_endpoint
+          block.call(args[0])
+        rescue Exception => e
+          puts e
+          raise
+        end
+      end
+
+      def create(attributes = {})
+        group = CgRoleClient::Group.new(attributes)
+        if !group.valid? || !group.id.nil?
+          return false
+        end
+        @endpoint.create_group(group)
+      end
+
+      def find_by_code(group_code)
+        @endpoint.find_group_by_code(group_code)
+      end
+
+    end
 
     def initialize(attributes = {})
       self.attributes = attributes
     end
+
+    def actors
+      begin
+        ensure_endpoint
+        @endpoint.find_group_actors_by_group_id(@id)
+      rescue Exception => e
+        puts e
+        raise
+      end
+    end
+
+    # Might be needed later
+=begin
+    def roles
+      @endpoint.find_group_roles_by_group_id
+    end
+=end
 
   end
 
