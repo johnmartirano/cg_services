ddfb5c82299eaf163b8aa5d2ceca0906a94abb6c
Andrew Shirk <andrew.shirk@commongroundpublishing.com>
2011-08-15 18:00:22 +0000

    Fixed misspelling
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@2125 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/rest_endpoint.rb
+++ lib/cg_service_client/rest_endpoint.rb
@@ -3,7 +3,7 @@ require 'typhoeus'
 module CgServiceClient
   # Generic base class for interacting with RESTFul service endpoints.
   class RestEndpoint
-    REQEUST_TIMEOUT = 5000 # milliseconds
+    REQUEST_TIMEOUT = 5000 # milliseconds
 
     attr_reader :uri, :version
 
