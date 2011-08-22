4eea5f8b7ccb3df1959a4dca43b299bf3750ed3c
John Martirano <john.martirano@commongroundpublishing.com>
2011-08-22 17:33:34 +0000

    Removing gem declares from cg_lookup_client.rb, these caused some conficts as they are declared outside the scope of what Bundler can see. Make sure to use bundle exec if dependencies are not found during rake tasks or other executables.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_lookup_client/trunk@2342 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_lookup_client.rb
+++ lib/cg_lookup_client.rb
@@ -1,11 +1,6 @@
 # This first block is required for Ruby 1.8.7. Once we've upgraded to 1.9.x,
 # it can be removed.
 require 'rubygems'
-gem 'rest-client', '=1.6.3'
-gem 'activemodel', '>=3.0.0'
-gem 'activesupport', '>=3.0.0'
-gem 'activerecord', '>=3.0.0'
-#changed from 3.0.6
 require 'cg_lookup_client/entry'
 require 'cg_lookup_client/rest_endpoint'
 
