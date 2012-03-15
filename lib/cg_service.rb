cd5ae5afbeaf923a3191ad19e01157c7894eeb77
Karthik Krishnan <karthik@mahaswami.com>
2012-03-15 09:03:59 +0000

    Introduced a method to cg_service to initialize Registration Thread to Lookup Service which reads the application url from service.yml.  The original start_registration_thread message is deprecated due to the fact that it accepts fixed set of parameters called host, port and config.  Its better to have all these information coming from config
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/services/cg_service/trunk@6149 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service.rb
+++ lib/cg_service.rb
@@ -17,9 +17,61 @@ module CgService
     end
   end
 
+  # Initialize a thread that will register with a lookup service according to
+  # +config+, which may be a
+  # Hash or a string filename that will be loaded using YAML.load_file
+  # The old method +start_registration_thread+ is deprecated and should not be used
+  # as it does not have facility to provide custom context_root and assumes the context_root
+  # to always be '/'
+  def self.init_registration_thread(config)
+    config = YAML.load_file(config) if config.kind_of? String
+
+    # Important to not require 'cg_lookup_client' until we're ready to
+    # start the registration thread.  This must be done after rackup
+    # daemonized so the registration thread runs in the child rather
+    # than dying in the parent.
+    require 'cg_lookup_client' # starts registration thread
+
+    url = config['application_service_url']
+    name = config['name'] || 'Unknown'
+    description = config['description'] || "#{name} Service"
+    version = config['version'] || '1'
+    lookup_service_uri = config['lookup_service_uri'] || 'http://localhost:5000/'
+    lookup_service_version = config['lookup_service_version'] || '1'
+
+    endpoint = CgLookupClient::RestEndpoint.new(lookup_service_uri,
+                                                lookup_service_version)
+    CgLookupClient::Entry.configure_endpoint(endpoint)
+
+    entry = CgLookupClient::Entry.new(:type_name => name,
+                                      :description => description,
+                                      :uri => url,
+                                      :version => version)
+
+    # The registration thread will warn when renewal fails.
+    # TODO: If renewal fails 3 or more times we should notify an
+    # admin.
+    fail_count = 0
+    entry.register do |status|
+      if !status[:success]
+        fail_count+=1
+        puts status[:message] + " --  Failed #{fail_count} times to connect to CgLookupService endpoint #{status[:endpoint]}."
+      else
+        fail_count=0
+        puts status[:message] + " --  Successfully renewed with CgLookupService endpoint #{status[:endpoint]}."
+      end
+      if fail_count >= 3
+        puts "TODO: An admin should be notified at this point."
+      end
+    end
+
+    puts "|| CG #{name} Service is starting up on #{url} ..."
+  end
+
   # Start a thread that will register with a lookup service according
   # to +host+, +port+, and various keys in +config+, which may be a
   # Hash or a string filename that will be loaded using YAML.load_file
+  # This method is deprecated, please use +init_registration_thread+ instead.
   def self.start_registration_thread(host, port, config)
     config = YAML.load_file(config) if config.kind_of? String
 
@@ -90,12 +142,12 @@ module CgService
     end
 
     # Configure database to environment based on:
-    # checks for -e flag, then ENV["SINATRA_ENV"], then defaults to development
+    # checks for -e flag, then ENV["RACK_ENV"], then ENV["SINATRA_ENV"], then defaults to development
     def configure_db(db_file)
       configure do
         env_arg_idx = ARGV.index("-e")
         env_arg = ARGV[env_arg_idx+1] unless env_arg_idx == nil
-        env = env_arg || ENV["SINATRA_ENV"] || "development"
+        env = env_arg || ENV["RACK_ENV"] || ENV["SINATRA_ENV"] || "development"
         databases = YAML.load_file(db_file)
         ActiveRecord::Base.establish_connection(databases[env])
         ActiveRecord::Base.include_root_in_json = false
@@ -132,10 +184,20 @@ module CgService
     # Start a thread that will register with a lookup service according
     # to +host+, +port+, and various keys in +config+, which may be a
     # Hash or a string filename that will be loaded using YAML.load_file
+    # This method is deprecated, please use +init_registration_thread+ instead
     def start_registration_thread(host, port, config)
       CgService.start_registration_thread(host, port, config)
     end
 
+    # Initialize a thread that will register with a lookup service according to
+    # +config+, which may be a
+    # Hash or a string filename that will be loaded using YAML.load_file
+    # The old method +start_registration_thread+ is deprecated and should not be used
+    # as it does not have facility to provide custom context_root and assumes the context_root
+    # to always be '/'
+    def init_registration_thread(host, port, config)
+      CgService.init_registration_thread(config)
+    end
   end
 
 end
