f9e33b64d75a7c694ff174d623ab0787ca6a4b46
Patrick Mahoney <patrick.mahoney@commongroundpublishing.com>
2012-04-11 16:57:07 +0000

    Really merge cg_service_client jruby branch.  See CP-1252.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@6938 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/rest_endpoint.rb
+++ lib/cg_service_client/rest_endpoint.rb
@@ -36,6 +36,65 @@ module CgServiceClient
 
     protected
 
+    def run_request(request_url, request_options = {}, options = {}, &block)
+			if defined?(JRUBY_VERSION)
+        run_rest_client_request(request_url, request_options, options, &block)
+			else
+			  request = Typhoeus::Request.new(request_url, request_options)
+				run_typhoeus_request(request, options, &block)
+			end
+    end
+
+    def rest_client_cache_key(url)
+			Digest::SHA1.hexdigest(url)
+    end
+
+		def run_rest_client_request(request_url, request_options = {}, options = {}, &block)
+			options = {:only_cache_200s => true}.merge(options)
+
+      if request_options[:method] == :get
+        response = @cache.get(rest_client_cache_key(request_url)) rescue nil
+      end
+
+      request_options[:timeout] ||= REQUEST_TIMEOUT
+      timeout = (request_options[:timeout] / 1000)
+      request_options[:headers].merge!({:params => request_options.delete(:params)}) if request_options[:params]
+      request = RestClient::Request.new({:url => request_url,
+                                         :method => request_options[:method],
+                                         :headers => request_options[:headers],
+                                         :payload => request_options[:body],
+                                         :timeout => timeout})
+
+      if response.nil?
+        begin
+          response = request.execute
+        rescue RestClient::RequestTimeout => e
+          raise CgServiceClient::Exceptions::TimeoutError.new(nil, nil), "Request for #{request_url} timed out."
+        end
+      end
+
+      ret = nil
+      if (response.code >= 200 && response.code < 300)
+        if (request_options[:method] == :get && request_options[:cache_timeout] && cacheable?(response, options))
+          @cache.set(rest_client_cache_key(request_url), response, request_options[:cache_timeout])
+        end
+        ret = yield response
+      elsif response.code >= 400 && response.code < 500
+        raise CgServiceClient::Exceptions::ClientError.new(response.code, response.description), "Client error #{response.code}: #{response.body}."
+      elsif response.code >= 500
+        raise CgServiceClient::Exceptions::ServerError.new(response.code, response.description), "Server error #{response.code}: #{response.body}."
+      elsif response.code == 0
+        # no http response
+        raise CgServiceClient::Exceptions::ConnectionError.new(response.code, response.description), response.description
+      #elsif response.timed_out?
+      #  raise CgServiceClient::Exceptions::TimeoutError.new(response.code, response.curl_error_message), "Request for #{request_url} timed out."
+      else
+        raise CgServiceClient::Exceptions::ConnectionError.new(response.code, response.body), "Request for #{request_url} failed."
+      end
+
+			ret
+		end
+
       # Returns the result of the block on success.
       # Options:
       #     :only_cache_200s Whether or not to only cache responses that return a 200
@@ -80,9 +139,9 @@ module CgServiceClient
       ret
     end
 
-    def cacheable?(request, options)
+    def cacheable?(response, options)
       !options[:only_cache_200s] ||
-          (options[:only_cache_200s] && request.response.code >= 200 && request.response.code < 300)
+          (options[:only_cache_200s] && response.code >= 200 && response.code < 300)
     end
   end
 
