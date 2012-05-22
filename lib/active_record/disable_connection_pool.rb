27574b5b39d07c56234b20ac58c8ed1365399257
Patrick Mahoney <patrick.mahoney@commongroundpublishing.com>
2012-05-22 21:24:43 +0000

    Formatting. Convert tabs to spaces.
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/services/cg_service/trunk@7576 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/active_record/disable_connection_pool.rb
+++ lib/active_record/disable_connection_pool.rb
@@ -1,4 +1,4 @@
-if RUBY_PLATFORM =~ /java/ and ENV['RACK_ENV'] == 'production'
+if RUBY_PLATFORM =~ /java/ and ENV['RACK_ENV'] != 'development'
   class ActiveRecord::ConnectionAdapters::ConnectionPool
     def checkout
       # Checkout an available connection
@@ -9,10 +9,10 @@ if RUBY_PLATFORM =~ /java/ and ENV['RACK_ENV'] == 'production'
 
     def checkin(conn)
       @connection_mutex.synchronize do
-				conn.send(:_run_checkin_callbacks) do
-					@checked_out.delete conn
-					@queue.signal
-				end
+        conn.send(:_run_checkin_callbacks) do
+          @checked_out.delete conn
+          @queue.signal
+        end
         @connections.delete(conn)
         conn.disconnect!
       end
