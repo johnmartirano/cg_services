85149a94f9b6138b0d7f5c1d5ebc28dd97675d21
Walker Weyerhaeuser <walker.weyerhaeuser@commongroundpublishing.com>
2012-08-13 19:33:25 +0000

    CP-1912 remove ensure_endpoint and add endpoint
    git-svn-id: svn+ssh://build.commongroundpublishing.com/srv/svn/repos/cgp/framework/cg_service_client/trunk@8888 bc291798-7d79-44a5-a816-fbf4f7d05ffa

--- lib/cg_service_client/serviceable.rb
+++ lib/cg_service_client/serviceable.rb
@@ -6,8 +6,6 @@ module CgServiceClient
   # extend CgServiceClient::Serviceable
   # Then, call uses_service to set the initialization variables. endpoint_class
   # should be a subclass of CgServiceClient::RestEndpoint.
-  # Finally, at the beginning of any method that makes calls on the endpoint, make sure
-  # to call ensure_endpoint.
   module Serviceable
     ENDPOINT_FLUSH_INTERVAL_IN_SEC = 60
 
@@ -23,14 +21,11 @@ module CgServiceClient
       @endpoint_class = endpoint_class
     end
 
-    # CgServiceClient::RestEndpoint manages the relationship with lookup client,
+    # CP-1912  CgServiceClient::RestEndpoint manages the relationship with lookup client,
     # and maintains a pool of endpoints for all the services on all the node agents
     # this way when any class notices a bad endpoint it can be refreshed for all classes
-    def ensure_endpoint
-      @endpoint = CgServiceClient::RestEndpoint.get(@service_name, @service_version, @endpoint_class)
-    end
-
-    class ServiceUnavailableError < StandardError;
+    def endpoint
+      CgServiceClient::RestEndpoint.get(@service_name, @service_version, @endpoint_class)
     end
 
   end
